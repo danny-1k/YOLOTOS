36d869c5f14a68471ac8fd3ad7ef38e9c8250ff3
Daniel Ik <codingeinstein@gmail.com>
2023-05-29 20:30:53 +0100

    vod dataset

---
+++ src/datasets/voc.py
@@ -0,0 +1,88 @@
+import torch
+from torch.utils.data import Dataset
+from .transforms import detection_transforms
+from torchvision.datasets.voc import VOCDetection
+
+
+def label_from_voc(vocab, annotations, width, height):
+    """Convert VOC annotations to YOLOTOS format for training
+
+    Args:
+        vocab : Vocab class
+        annotations : PascalVOC annotations
+        width : Width of image (Used for normalization)
+        height : Height of image (Used for normalization)
+    """
+    classes = []
+    coords = []
+
+    for annotation in annotations:
+        obj_class = annotation['name']
+        obj_bbox = annotation['bndbox']
+
+        xmin = int(obj_bbox['xmin'])
+        xmax = int(obj_bbox['xmax'])
+        ymin = int(obj_bbox['ymin'])
+        ymax = int(obj_bbox['ymax'])
+
+        x_center = ((xmin + xmax)//2)/width  # normalized x_center
+        y_center = ((ymin + ymax)//2)/height  # normalized y_center
+
+        bb_width = (xmax - xmin)/width  # normalized bb width
+        bb_height = (ymax - ymin)/height  # normalized bb height
+
+        classes.append(obj_class)
+        coords.append([x_center, y_center, bb_width, bb_height])
+
+    classes = vocab.encode(classes)
+    coords = [
+        [1, 1, 1, 1],
+        *coords,
+        [0, 0, 0, 0]
+    ]  # SOS should be [1, 1, 1, 1] and EOS is [0, 0, 0, 0]
+
+    # it doesn't matter much what EOS is for the regression case
+    # as when the model is done is dependent on the classification outputs
+
+    return classes, coords, len(classes)
+
+
+class PascalVocDataset(Dataset):
+    def __init__(self, vocab, root="../data/voc", download=False, year="2007", image_set="val", shuffle=None) -> None:
+        self.vocab = vocab
+        self.download = download
+        self.year = year
+        self.image_set = image_set
+        self.train = True if "train" in image_set else "test"
+        self.transform = detection_transforms["train" if self.train else "test"]
+        self.shuffle = shuffle or self.train
+
+        self.dataset = VOCDetection(
+            root=root, year=year,
+            image_set=image_set,
+            transform=self.transform,
+            download=download
+
+        )
+
+    def __getitem__(self, index):
+        image, annotation = self.dataset[index]
+
+        annotation = annotation["annotation"]
+
+        w = int(annotation['size']['width'])
+        h = int(annotation['size']['height'])
+
+        annotations = annotation['object']
+
+        classes, coords, length = label_from_voc(
+            vocab=self.vocab,
+            annotations=annotations,
+            width=w, height=h,
+            shuffle=False
+        )
+
+        return image, torch.Tensor(classes), torch.Tensor(coords), length
+
+    def __len__(self):
+        return len(self.dataset)
