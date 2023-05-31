379c3a899693c45983069e4e0de66fc40ebf8fff
Daniel Ik <codingeinstein@gmail.com>
2023-05-31 17:48:20 +0100

    model utils

---
+++ src/utils/model.py
@@ -0,0 +1,22 @@
+import torch
+
+def clean_prediction(classes, bbs):
+    """Clean output of model - Remove EOS & padding tokens, etc
+
+    Args:
+        classes : Tensor of shape (N,) where N is the number of objects detected (Including EOS & PAD)
+        bbs : Tensor of shape (N, 4). N must be equal to N of `classes`.
+
+    """
+
+    N = classes.shape[0]
+    indices = torch.arange(N)
+    # indices 0, 1  correspond to <s>, </s> and </p> respectively
+    mask = (classes != 0) & (classes != 1)
+
+    indices = indices[mask]
+
+    classes = classes[indices]
+    bbs = bbs[indices]
+
+    return classes, bbs
