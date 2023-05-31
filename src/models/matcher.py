d811ec2130ae9012228dc8a699d788595be000ce
Daniel Ik <codingeinstein@gmail.com>
2023-05-31 12:08:17 +0100

    modified HungarianMatcher class

--- src/models/matcher.py
+++ src/models/matcher.py
@@ -1,15 +1,15 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-Modules to compute the matching cost and solve the corresponding LSAP.
-"""
 import torch
 from scipy.optimize import linear_sum_assignment
 from torch import nn
 
-from ..utils.box_ops import box_cxcywh_to_xyxy, generalized_box_iou
+import sys
+sys.path.append("..")
+from utils.box_ops import box_cxcywh_to_xyxy, generalized_box_iou
 
 
 class HungarianMatcher(nn.Module):
+    # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
     """This class computes an assignment between the targets and the predictions of the network
 
     For efficiency reasons, the targets don't include the no_object. Because of this, in general,
@@ -88,5 +88,102 @@ class HungarianMatcher(nn.Module):
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
+class HungarianMatcher2(nn.Module):
+    # Addapted from FB research DETR official implementation
+
+    def __init__(self, cost_class: float = 1, cost_bbox: float = 1, cost_giou: float = 1):
+        super().__init__()
+
+        self.cost_class = cost_class
+        self.cost_bbox = cost_bbox
+        self.cost_giou = cost_giou
+        assert cost_class != 0 or cost_bbox != 0 or cost_giou != 0, "all costs cant be 0"
+
+    def forward(self, predictions, targets):
+        """Performs the Matching
+
+        Args:
+            predictions : This is a dict that contains at least these entries:
+                 "classes": Tensor of dim [batch_size, num_predictions, num_tokens] with the classification logits
+                 "bbs": Tensor of dim [batch_size, num_predictions, 4] with the predicted box coordinates
+
+            targets : This is a dict that contains at least these entries:
+                 "classes": Tensor of dim [batch_size, num_predictions] with the classification logits
+                 "bbs": Tensor of dim [batch_size, num_predictions, 4] with the predicted box coordinates
+
+        Returns:
+            A dict with entries:
+                "predictions": Tensor of dim [batch_size, num_predictions] with the indices of the selected predictions (in order)
+                "targets": Tensor of dim [batch_size, num_predictions] with the indices of the selected targets (in order)
+        """
+
+
+        predicted_bbs = predictions["bbs"]
+        predicted_classes = predictions["classes"]
+
+        target_bbs = targets["bbs"]
+        target_classes = targets["classes"]
+
+        bs, num_predictions = predicted_bbs.shape[:2]
+
+        predicted_bbs = predicted_bbs.view(bs*num_predictions, -1) # flatten & softmax -> (N * S, 4)
+        predicted_classes = predicted_classes.view(bs*num_predictions, -1).softmax(-1) # flatten & softmax -> (N * S, n_tokens)
+
+        target_bbs = target_bbs.view(bs*num_predictions, -1)
+        target_classes = target_classes.view(bs*num_predictions)
+
+        cost_class = -predicted_classes[:, target_classes]
+
+        cost_bbs = torch.cdist(predicted_bbs, target_bbs, p=1) # euclid distance
+
+        cost_giou = generalized_box_iou(box_cxcywh_to_xyxy(
+                predicted_bbs), box_cxcywh_to_xyxy(target_bbs)) #giou cost
+        
+        # cost matrix
+
+        C = self.cost_bbox * cost_bbs + self.cost_class * \
+            cost_class + self.cost_giou * cost_giou
+        C = C.view(bs, num_predictions, -1).cpu()
+
+        sizes = tuple(num_predictions for _ in range(bs))
+
+        indices = [linear_sum_assignment(c[i])
+                   for i, c in enumerate(C.split(sizes, -1))]
+        
+
+        prediction_indices, target_indices = list(zip(*indices))
+
+        prediction_indices = torch.as_tensor(prediction_indices)
+        target_indices = torch.as_tensor(target_indices)
+
+        return {
+            "predictions": prediction_indices,
+            "targets": target_indices
+        }
+
+
+
+
 def build_matcher(args):
-    return HungarianMatcher(cost_class=args.set_cost_class, cost_bbox=args.set_cost_bbox, cost_giou=args.set_cost_giou)
+    return HungarianMatcher2(cost_class=args.set_cost_class, cost_bbox=args.set_cost_bbox, cost_giou=args.set_cost_giou)
+
+
+if __name__ == "__main__":
+
+    matcher = HungarianMatcher2()
+
+    predictions = {
+        "bbs": torch.rand(1, 3, 4).sigmoid(),
+        "classes": torch.Tensor(1, 3, 5)
+    }
+
+    targets = {
+        "bbs": torch.rand(1, 3, 4).sigmoid(),
+        "classes": torch.randint(0, 5, (1, 3)).long()
+    }
+
+    lengths = torch.Tensor([2, 3, 3])
+
+    print(targets["bbs"])
+
+    indices = matcher(predictions, targets)
