c06c3ca8d27e12f011837a0330fd3a0e32e8a3f7
Daniel Ik <codingeinstein@gmail.com>
2023-06-03 23:06:41 +0100

    add scripts

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,16 @@
+cd ../src
+
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 256 --S 16 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 256 --S 32 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 256 --S 64 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 256 --S 128 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 512 --S 16 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 512 --S 32 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 512 --S 64 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 512 --S 128 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 1024 --S 16 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 1024 --S 32 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 1024 --S 64 --lr 3e-4 --weight_decay 0 --batch_size 64 &
+nohup python train.py --device cuda --num_workers 2 --visualize_attention --hidden_size 1024 --S 128 --lr 3e-4 --weight_decay 0 --batch_size 64 &
